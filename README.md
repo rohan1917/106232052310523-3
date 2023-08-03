530d71c4330633c395f1badc4c8b1d8d8b9edcd9
Rohan Kinnal <64979784+rohan1917@users.noreply.github.com>
2023-08-03 14:56:50 +0000

    getPostDelete

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # 106232052310523-3
+
+
+http://localhost:8081/
+http://localhost:8081/create
+http://localhost:8081/home
+http://localhost:8081/login
+http://localhost:8081/signup
+
+
+## HTTP Methods
+GET       ||   Get certain amount of data from the server    
+PUT       ||   Overwriting, full update
+DELETE    ||   Deleting data from the server
+PATCH     ||   upadting certain feilds
+POST      ||   Sending data to the server
+
+
+## Server Codes
+200 || Ok
+400 || Server Error
+404 || Server Not Found
+401 || Page Not Found
+201 || Insertion or Creation
+501 || Not Implemented
+
+
+ThunderClient
+Postman
+Insomina
--- server.js
+++ server.js
@@ -1,15 +1,66 @@
-const http = require("http");  // package import
+const http = require("http");  
 
-const port = 8081; //port number
+const port = 8081; 
 
-http.createServer((request, response)=>{
-    // callback func
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<h1>Hello world, server started, hi </h1>");
-    response.end();
+const toDoList = ["java", "javaScript", "Python"];
+
+http.createServer((req, res)=>{
+    const {method, url} = req;
+    // console.log(method, url);
+
+    if(url === '/todos'){
+        if(method === 'GET'){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(toDoList.toString());
+        }else if (method === 'POST'){
+            let body = "";
+            req.on('error',(err)=>{
+                console.log(err);
+            }).on('data', (chunk)=>{
+                body += chunk;
+                // console.log(chunk);
+            }).on('end', ()=>{
+                body=JSON.parse(body)
+                // console.log("complete data: ", body);
+                let newToDo = toDoList;
+                newToDo.push(body.item);
+                console.log(newToDo);
+                // res.writeHead(201);
+            })
+        } else if (method === 'DELETE'){
+            let body = '';
+            req.on('error', (err)=>{
+                console.log(err)
+            }).on('data', (chunk)=>{
+                body+= chunk
+            }).on('end', ()=>{
+                body = JSON.parse(body);
+                let deleteThis = body.item;
+                // for(let i=0 ; i<toDoList.length; i++){
+                //     if(toDoList[i]===deleteThis){
+                //         toDoList.splice(i,1);
+                //         break;
+                //     }
+                // }
+
+                toDoList.find((elem, index)=>{
+                    if(elem === deleteThis ){
+                        toDoList.splice(index, 1);
+                    }
+                })
+            })
+        }
+        
+
+    } else if (url === '/'){
+        res.writeHead(200, {"Content-Type": "text/html"} );
+        res.write("<h1>Default Page</h1>");
+    }else{
+        res.writeHead(501);
+    }
+    res.end();
 })
 .listen(port, ()=>{
-    // callback func
     console.log(`Nodejs Server started running on port ${port}`);
 });
 
